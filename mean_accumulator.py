8979549f8540bd5833c532a673ce1cfdcb043928
Kurt Ehlert <kehlert@wisc.edu>
2014-11-18 10:00:46 -0600

    Added a MeanAccumulator for calculating the tau leaping means. Birth model mean matches the ODE solution, so it seems to work fine.

---
+++ mean_accumulator.py
@@ -0,0 +1,16 @@
+class MeanAccumulator:
+    def __init__(self):
+        self._data = {}
+
+    def add(self, newData):
+        for key, val in newData.items():
+            if key not in self._data:
+                self._data[key] = []
+            self._data[key].append(val)
+
+    def getMeans(self):
+        means = {}
+        for key in self._data:
+            keyData = self._data[key]
+            means[key] = sum(keyData)/len(keyData)
+        return means
--- test_tau_leaper.py
+++ test_tau_leaper.py
@@ -1,6 +1,8 @@
 import unittest
+import numpy
 from tau_leaper import TauLeaper
 from birth_model import BirthModel
+from mean_accumulator import MeanAccumulator
 
 
 class TestConstructor(unittest.TestCase):
@@ -9,10 +11,23 @@ class TestConstructor(unittest.TestCase):
         endTime = 1
         TauLeaper(BirthModel(), stepSize, endTime)
 
-    def testRun(self):
+    def testSingleRun(self):
         stepSize = 0.001
         endTime = 5
         model = BirthModel()
         myLeaper = TauLeaper(model, stepSize, endTime)
         results = myLeaper.run()
         self.assertGreater(results['X'], model.getInitialState()['X'])
+
+    def testMean(self):
+        stepSize = 0.001
+        endTime = 5
+        model = BirthModel()
+        myLeaper = TauLeaper(model, stepSize, endTime)
+
+        accum = MeanAccumulator()
+        nRuns = 10
+        numpy.random.seed(0)
+        for i in range(0, nRuns):
+            accum.add(myLeaper.run())
+        self.assertAlmostEqual(16.2, accum.getMeans()['X'])
